0e6e319805c8488da8ff891bba539bd933e040ae
AbdelGad <abdelgad@hotmail.com>
2023-03-02 18:53:01 +0100

    Add core app to the project

--- src/steam/steam/settings.py
+++ src/steam/steam/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 
     # 3rd party
     'django_plotly_dash.apps.DjangoPlotlyDashConfig',
