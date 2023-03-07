4bd795c677bf1916dea083f5f5849c59506dba87
AbdelGad <abdelgad@hotmail.com>
2023-03-07 18:02:26 +0100

    Add list projects
    Co-authored-by: abdelgad <abdelgad@hotmail.com>
    Co-authored-by: manalelk <manalelkaidi@hotmail.com>

--- src/steam/core/urls.py
+++ src/steam/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProjectCreateView, ProjectUpdateView
+from .views import ProjectListView, ProjectCreateView, ProjectUpdateView
 
 urlpatterns = [
+    path('projects/', ProjectListView.as_view(), name='project-list'),
     path('project/create/', ProjectCreateView.as_view(), name='project-create'),
     path('project/<int:pk>/update/',
          ProjectUpdateView.as_view(), name='project-update'),
--- src/steam/core/views.py
+++ src/steam/core/views.py
@@ -1,15 +1,21 @@
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Project
 from django.contrib.auth.models import User
 from .forms import ProjectForm
 
 
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'project/list.html'
+    context_object_name = 'projects'
+
+
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectForm
     template_name = 'project/create_update.html'
-    # success_url = reverse_lazy('project-list')
+    success_url = reverse_lazy('project-list')
 
     def get_context_data(self, **kwargs):
         context = super(ProjectCreateView, self).get_context_data(**kwargs)
@@ -21,7 +27,7 @@ class ProjectUpdateView(UpdateView):
     model = Project
     form_class = ProjectForm
     template_name = 'project/create_update.html'
-    # success_url = reverse_lazy('project-list')
+    success_url = reverse_lazy('project-list')
 
     def get_context_data(self, **kwargs):
         context = super(ProjectUpdateView, self).get_context_data(**kwargs)
