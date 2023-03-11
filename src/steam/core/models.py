5536f7b410e305cecee3758be7d32f1e14a73839
AbdelGad <abdelgad@hotmail.com>
2023-03-11 22:29:48 +0100

    Add use of django forms in create_update.html template
    Co-authored-by: abdelgad <abdelgad@hotmail.com>
    Co-authored-by: manalelk <manalelkaidi@hotmail.com>

--- src/steam/core/models.py
+++ src/steam/core/models.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     start_date = models.DateField()
     end_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,14 +13,15 @@ class Project(models.Model):
     is_archived = models.BooleanField(default=False)
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='owned_projects')
-    members = models.ManyToManyField(User, related_name='projects')
+    members = models.ManyToManyField(User, related_name='projects', blank=True)
 
     def __str__(self):
         return self.name
 
+    # TODO: localization of validation errors
     def clean(self):
         if self.start_date > self.end_date:
-            raise ValidationError('Start date must be before end date.')
+            raise ValidationError('La date de début doit être antérieure à la date de fin.')
 
     # clean() is not automatically called when an object is saved, hence the overriding of save()
     def save(self, *args, **kwargs):
