d6af4f9a3ffd5e2ca859975af57d7b3341c77771
AbdelGad <abdelgad@hotmail.com>
2023-03-11 22:29:48 +0100

    Add use of django forms in create_update.html template
    Co-authored-by: abdelgad <abdelgad@hotmail.com>
    Co-authored-by: manalelk <manalelkaidi@hotmail.com>

--- src/steam/core/forms.py
+++ src/steam/core/forms.py
@@ -4,30 +4,27 @@ from .models import Project
 
 # TODO: use form in template
 class ProjectForm(forms.ModelForm):
-    members = forms.ModelMultipleChoiceField(
-        queryset=User.objects.all(),
-        widget=forms.CheckboxSelectMultiple(),
-        required=False
-    )
-
     class Meta:
         model = Project
-        fields = [
-            'name',
-            'description',
-            'start_date',
-            'end_date',
-            'is_archived',
-            'owner',
-            'members'
-        ]
-        widgets = {
-            'start_date': forms.DateInput(attrs={'type': 'date'}),
-            'end_date': forms.DateInput(attrs={'type': 'date'}),
-            'owner': forms.Select(attrs={'class': 'form-control'}),
+        fields = '__all__'
+        # TODO: localization of labels
+        labels = {
+            'name': 'Nom',
+            'description': 'Description',
+            'start_date': 'Date de début',
+            'end_date': 'Date de fin',
+            'is_archived': 'Archivé',
+            'owner': 'Propriétaire',
+            'members': 'Membres'
         }
-
-    def __init__(self, *args, **kwargs):
-        super(ProjectForm, self).__init__(*args, **kwargs)
-        # self.fields['members'].queryset = User.objects.all().exclude(id=self.instance.owner.id)
-        self.fields['members'].queryset = User.objects.all()
+        widgets = {
+            # TODO: localization of placeholders
+            'name': forms.TextInput(attrs={'placeholder': 'Nom du projet', 'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'placeholder': 'Description du projet', 'class': 'form-control', 'rows': 3}),
+            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'is_archived': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'owner': forms.Select(attrs={'class': 'form-select'}),
+            # TODO: add widget with search option
+            'members': forms.SelectMultiple(attrs={'class': 'form-select'})
+        }
--- src/steam/core/views.py
+++ src/steam/core/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Project
-from django.contrib.auth.models import User
 from .forms import ProjectForm
 
 
@@ -17,22 +16,12 @@ class ProjectCreateView(CreateView):
     template_name = 'project/create_update.html'
     success_url = reverse_lazy('project-list')
 
-    def get_context_data(self, **kwargs):
-        context = super(ProjectCreateView, self).get_context_data(**kwargs)
-        context['users'] = User.objects.all()
-        return context
-
 
 class ProjectUpdateView(UpdateView):
     model = Project
     form_class = ProjectForm
     template_name = 'project/create_update.html'
     success_url = reverse_lazy('project-list')
-
-    def get_context_data(self, **kwargs):
-        context = super(ProjectUpdateView, self).get_context_data(**kwargs)
-        context['users'] = User.objects.all()
-        return context
 
 
 class ProjectDeleteView(DeleteView):
