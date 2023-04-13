55ba54bb0e2aa83122536752ec6ed79c21b7ac92
abdelgad <59240245+abdelgad@users.noreply.github.com>
2023-04-13 10:59:06 +0200

    Add import of RegexValidator in models.py

--- src/steam/core/models.py
+++ src/steam/core/models.py
@@ -3,10 +3,7 @@ import string
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
-from django.db.models.signals import m2m_changed
-from django.dispatch import receiver
-from django.core.validators import RegexValidator, EmailValidator
-from django.conf import settings
+from django.core.validators import RegexValidator
 
 
 class Project(models.Model):
