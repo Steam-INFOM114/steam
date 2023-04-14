d5c3777de4e087ac4f990d98ed27b1fcb31ac9e4
nbaltofs <90826197+nbaltofs@users.noreply.github.com>
2023-04-14 12:50:42 +0200

    STEAM-34 tests link task project (#17)
    * Add test for link between Task and Project (model).
    Add some missing tests for Task (model)
    
    * Add test for link between Task and Project (form).
    Add some missing tests for Task (form)
    
    * Improve tests and add missing tests for link between task and project (model & form)
    
    * Add test for link between Task and Project (view create).
    Add some missing tests for Task (view create)
    
    * Add test for link between Task and Project (view update).
    Add some missing tests for Task (view update)
    
    * Fix import
    
    * Add tests when Task is not found
    
    * Add tests for view Task detail
    
    * Remove unwanted check
    
    Co-authored-by: abdelgad <59240245+abdelgad@users.noreply.github.com>
    
    * Remove unwanted check
    
    Co-authored-by: abdelgad <59240245+abdelgad@users.noreply.github.com>
    
    * Remove duplicated tests
    
    Co-authored-by: abdelgad <59240245+abdelgad@users.noreply.github.com>
    
    * Improve test
    
    Co-authored-by: abdelgad <59240245+abdelgad@users.noreply.github.com>
    
    * Update task list path
    
    Co-authored-by: abdelgad <59240245+abdelgad@users.noreply.github.com>
    
    * Use valid date format
    
    Co-authored-by: abdelgad <59240245+abdelgad@users.noreply.github.com>
    
    * Modify count verification number after updating a task
    
    Co-authored-by: nbaltofs <90826197+nbaltofs@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: abdelgad <59240245+abdelgad@users.noreply.github.com>

--- src/steam/core/tests/test_task_form.py
+++ src/steam/core/tests/test_task_form.py
@@ -72,3 +72,115 @@ class TaskFormTest(TestCase):
         self.assertFalse(form.is_valid())
         # Expect an error for the status field
         self.assertIn('status', form.errors)
+
+    def test_task_form_start_date_before_project_start_date_error(self):
+        """Test that the create task form is invalid with a start_date before the project start_date."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['start_date'] = self.project.start_date - \
+            timezone.timedelta(days=1)  # start_date before project start_date
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+
+    def test_task_form_end_date_after_project_end_date_error(self):
+        """Test that the create task form is invalid with an end_date after the project end_date."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['end_date'] = self.project.end_date + \
+            timezone.timedelta(days=1)  # end_date after project end_date
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+
+    def test_task_form_no_project_error(self):
+        """Test that the create task form is invalid with no project."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['project'] = None
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+        self.assertIn('project', form.errors)
+
+    def test_task_form_no_name_error(self):
+        """Test that the create task form is invalid with no name."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['name'] = ''
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+        self.assertIn('name', form.errors)
+
+    def test_task_form_name_only_spaces_error(self):
+        """Test that the create task form is invalid with a name that only contains spaces."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['name'] = '   '
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+        self.assertIn('name', form.errors)
+
+    def test_task_form_name_too_long_error(self):
+        """Test that the create task form is invalid with a name that is too long."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['name'] = 'a' * 101
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+        self.assertIn('name', form.errors)
+
+    def test_task_form_start_date_but_no_end_date_error(self):
+        """Test that the create task form is invalid with a start_date but no end_date."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['end_date'] = ''
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+        self.assertIn('end_date', form.errors)
+
+    def test_task_form_end_date_but_no_start_date_error(self):
+        """Test that the create task form is invalid with an end_date but no start_date."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['start_date'] = ''
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+        self.assertIn('start_date', form.errors)
+
+    def test_task_form_start_date_after_project_end_date_error(self):
+        """Test that the create task form is invalid with a start_date after the project end_date."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['start_date'] = self.project.end_date + \
+            timezone.timedelta(days=1)
+        invalid_data['end_date'] = invalid_data['start_date'] + \
+            timezone.timedelta(days=1)
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+
+    def test_task_form_end_date_before_project_start_date_error(self):
+        """Test that the create task form is invalid with an end_date before the project start_date."""
+        invalid_data = self.valid_data.copy()
+        invalid_data['start_date'] = self.project.start_date - \
+            timezone.timedelta(days=2)
+        invalid_data['end_date'] = self.project.start_date - \
+            timezone.timedelta(days=1)
+        form = TaskForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
+
+    def test_task_form_start_date_equals_end_date_is_valid(self):
+        """Test that the create task form is valid with a start_date equal to the end_date."""
+        self.valid_data['start_date'] = self.valid_data['end_date']
+        form = TaskForm(data=self.valid_data)
+        self.assertTrue(form.is_valid())
+
+    def test_task_form_start_date_equals_project_start_date_is_valid(self):
+        """Test that the create task form is valid with a start_date equal to the project start_date."""
+        self.valid_data['start_date'] = self.project.start_date
+        form = TaskForm(data=self.valid_data)
+        self.assertTrue(form.is_valid())
+
+    def test_task_form_end_date_equals_project_end_date_is_valid(self):
+        """Test that the create task form is valid with an end_date equal to the project end_date."""
+        self.valid_data['end_date'] = self.project.end_date
+        form = TaskForm(data=self.valid_data)
+        self.assertTrue(form.is_valid())
