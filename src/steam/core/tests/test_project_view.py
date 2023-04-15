3a5318e5805acdc7d85b93cd6a720afd1b7c30b7
nbaltofs <90826197+nbaltofs@users.noreply.github.com>
2023-04-15 15:47:07 +0200

    Use custom user model in tests project (#19)

--- src/steam/core/tests/test_project_view.py
+++ src/steam/core/tests/test_project_view.py
@@ -1,8 +1,10 @@
 from datetime import date
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from core.models import Project
+
+User = get_user_model()
 
 class TestProjectView(TestCase):
     def setUp(self):
