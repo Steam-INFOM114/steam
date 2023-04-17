e435a9b9631c98b7041bd2eeaf2ac8f1e13729ed
abdelgad <59240245+abdelgad@users.noreply.github.com>
2023-04-17 13:19:07 +0200

    Overall improvements to CRUD project (#7)
    * Add use of django forms in create_update.html template
    
    Co-authored-by: abdelgad <abdelgad@hotmail.com>
    Co-authored-by: manalelk <manalelkaidi@hotmail.com>
    
    * Fix datatable error when projects list is empty
    
    Co-authored-by: abdelgad <abdelgad@hotmail.com>
    Co-authored-by: manalelk <manalelkaidi@hotmail.com>
    
    * Add delete project (modal) template file
    
    Co-authored-by: abdelgad <abdelgad@hotmail.com>
    Co-authored-by: manalelk <manalelkaidi@hotmail.com>
    
    * Fix undefind error in Project model
    
    * Add detail project
    
    * Add authorization rules
    
    * Add new input and search bar for members input on project/create_update.html
    
    * Add project.start_date not equals project.end_date verification
    
    * Add project owner cannot be a member constraint
    
    * Add message for empty members lists in project/detail.html and project/create_update.html
    
    * Add project owner cannot be a member constraint on owner update
    
    * Fix migrations conflict
    
    ---------
    
    Co-authored-by: manalelk <manalelkaidi@hotmail.com>

--- src/steam/core/urls.py
+++ src/steam/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProjectListView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView, TaskDetail, TaskCreate, TaskUpdate, TaskList, TaskDeleteView, loginPage, logoutUser, registerPage
+from .views import ProjectListView, ProjectDetailView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView, TaskDetail, TaskCreate, TaskUpdate, TaskList, TaskDeleteView, loginPage, logoutUser, registerPage
 
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('logout/', logoutUser, name="logout"),
     path('register/', registerPage, name="register"),
     path('projects/', ProjectListView.as_view(), name='project-list'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
     path('project/create/', ProjectCreateView.as_view(), name='project-create'),
     path('project/<int:pk>/update/',
          ProjectUpdateView.as_view(), name='project-update'),
