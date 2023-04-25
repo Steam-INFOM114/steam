b2774d910daba6c7ad25b6f3e721a9854f64ea90
manalelk <manalelkaidi@hotmail.com>
2023-04-26 00:44:13 +0200

    Add roles and permissions

--- src/steam/core/admin.py
+++ src/steam/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from core.models import MyUser
+from django.contrib.auth.models import Group
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs.filter(is_staff=True, is_superuser=False)
+        else:
+            return qs.none()
+
+admin.site.register(MyUser, CustomUserAdmin)
+admin.site.unregister(Group)
