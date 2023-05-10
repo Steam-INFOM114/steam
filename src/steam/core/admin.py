047169a8fbadad2fd846681fbb5764591efbf704
AbdelGad <abdelgad@hotmail.com>
2023-05-10 23:58:04 +0200

    Allow superadmin to manage non-admin user accounts

--- src/steam/core/admin.py
+++ src/steam/core/admin.py
@@ -14,7 +14,7 @@ class CustomUserAdmin(UserAdmin):
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
-            return qs.filter(is_staff=True, is_superuser=False)
+            return qs.filter(is_superuser=False)
         else:
             return qs.none()
 
