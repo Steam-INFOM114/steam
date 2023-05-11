67d2eba175b5f4ccbb7df52b417d1b2d7e75f261
abdelgad <59240245+abdelgad@users.noreply.github.com>
2023-05-11 10:41:16 +0200

    Admin page adjustments (#38)
    * Fix user creation through admin page
    
    * Allow superadmin to manage non-admin user accounts

--- src/steam/core/admin.py
+++ src/steam/core/admin.py
@@ -4,10 +4,17 @@ from core.models import MyUser
 from django.contrib.auth.models import Group
 
 class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff'),
+        }),
+    )
+
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
-            return qs.filter(is_staff=True, is_superuser=False)
+            return qs.filter(is_superuser=False)
         else:
             return qs.none()
 
